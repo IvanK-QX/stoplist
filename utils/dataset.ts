4c6ddbafd023dd747e91eeaee2058a27e7605aba
IvanK-QX <ivan.k@unitedtech.ai>
2023-12-15 09:13:17 +0200

    Add Prod Env Variables

--- utils/dataset.ts
+++ utils/dataset.ts
@@ -1,3 +1,4 @@
 export const dataSet = {
-    lambdaStoplistsUrl: 'https://2dy2uz7et5bwusqjdhgnbgivqu0qdmbm.lambda-url.us-east-1.on.aws/'
+    qaUrl: 'https://2dy2uz7et5bwusqjdhgnbgivqu0qdmbm.lambda-url.us-east-1.on.aws/',
+    prodUrl: 'https://u3h6t7yuxfexjplb6vrpyplbfy0kxvzx.lambda-url.us-east-1.on.aws/'
 }
